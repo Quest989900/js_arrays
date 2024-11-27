2d3678bd7d3d8ab8e21f27a5882aea82f67f07f1
quest989900 <quest98.99.00@gmail.com>
2024-11-27 16:11:55 +0100

    includes async...await

--- asychronousJS.js
+++ asychronousJS.js
@@ -1,22 +1,84 @@
 
-let result= fetch("https://api.coindesk.com/v1/bpi/currentprice.json")
+// let result= fetch("https://api.coindesk.com/v1/bpi/currentprice1.json")
+// console.log(result)
+// result.then((res)=>{
+//     let resParsed = res.json()
+//     // console.log(resParsed)
+//     return resParsed
+// })
+// .then((data)=>{
+//     console.log(data.bpi.USD.rate)
+// })
+// .catch((err)=>{
+//     console.log('I got hereeeeeeeee:', err)
+// })
 
-result.then((res)=>{
-    let resParsed = res.json()
-    // console.log(resParsed)
-    return resParsed
-})
-.then((data)=>{
-    console.log(data.bpi.USD.rate)
-})
-.catch((err)=>{
+
+// ASYNC.........AWAIT
+
+const getCoinRates = async ()=> {
+    try {
+        let result = await fetch("https://api.coindesk.com/v1/bpi/currentprice1.json")
+        let data = await result.json()
+        console.log(data.bpi.USD.rate)
+    }catch(err) {
+        console.log('I got hereeeeeeeee:', err)
+    }
+    
+}
+
+// getCoinRates()
+
+
+
+const getKhaalid = async ()=>{
+    try {
+        let res = await fetch("https://api.coindesk.com/v1/bpi/currentpric1e.json")
+        let data = await res.json()
+        console.log(data.bpi.USD.rate)
+        
+    }catch(err){
+        // console.log('This is errooooor!', err.message)
+        
+    }
+}
+
+getKhaalid()
+
+
+
+let outCome = fetch ("https://api.coindesk.com/v1/bpi/currentprice.json")
+ outCome.then((response) => {
+    let result = response.json()
+    return result
+ })
+ .then((ibro)=>{
+    // console.log(ibro.bpi.GBP.rate)
+ })
+ .catch((err)=>{
     console.log(err)
-})
+ })
+//  console.log(outCome)
+
+
+
+let inCome = async ()=>{
+try{
+    let result =await fetch ("https://api.coindesk.com/v1/bpi/currentprice.json")
+    let answer =await result.json()
+    let data = answer.bpi.USD.code
+    console.log(data)
+}
+catch(err){
+    console.log(err)
+}
+}
+
+inCome()
 
 
 
 
-console.log("AAAAAA")   
 
 
 
@@ -24,8 +86,10 @@ console.log("AAAAAA")
 
 
 
+// 'https://data.fixer.io/api/latest?access_key=d78782b1f9fd8844077ef50d3ae21a38'
 
 
+// "https://api.coindesk.com/v1/bpi/currentprice.json"
 
 
 
@@ -33,15 +97,19 @@ console.log("AAAAAA")
 
 // const apiUrl = 'https://api-football-v1.p.rapidapi.com/v2/odds/league/865927/bookmaker/5?page=2'
 
+
 // const options = {
-// 	method: 'GET',
+// 	method: 'POST',
 // 	headers: {
 // 		'x-rapidapi-key': 'a2cad8015emshd80ef224784c310p1fd101jsn98d74e332566',
-// 		'x-rapidapi-host': 'api-football-v1.p.rapidapi.com'
-// 	}
+// 		'x-rapidapi-host': 'google-translate1.p.rapidapi.com',
+// 		'Accept-Encoding': 'application/gzip'
+// 	},
+// 	// body: data
 // };
+// const url = 'https://data.fixer.io/api/latest?access_key=d78782b1f9fd8844077ef50d3ae21a38&symbols=NGN,GBP,USD';
 
-// let response = fetch(apiUrl, options)
+// let response = fetch(url)
 
 // response.then((res)=> {
 //     // console.log(res)
@@ -52,3 +120,5 @@ console.log("AAAAAA")
 // })
 
 // console.log("aaaaa")
+
+
---
+++ index.js
@@ -0,0 +1,45 @@
+const amount = document.querySelector('#amount')
+const currency = document.querySelector('#currency')
+const convertBtn = document.querySelector('#convert')
+const resetBtn = document.querySelector('#reset')
+// const result = document.querySelector('#result')
+
+
+const converter = async ()=> {
+    const url = 'https://data.fixer.io/api/latest?access_key=d78782b1f9fd8844077ef50d3ae21a38&symbols=NGN,GBP,USD';
+    let response = await fetch(url)
+    let data = await response.json()
+    console.log(data.rates)
+    return data.rates.NGN
+}
+
+// convertBtn.addEventListener('click', (e) => {
+//     e.preventDefault()
+//     // console.log(e,'I am clicked')
+//     document.querySelector('#result').innerText = 'A button is clicked'
+
+// })
+
+convertBtn.addEventListener('click', async(e) => {
+    e.preventDefault()
+    console.log(e,'I am clicked')
+    let output = await converter()
+    document.querySelector('#result').innerText = output
+    // document.querySelector('#result').innerHTML = '<h2>I am clicked</h2>'
+
+})
+
+
+// const converter = async ()=> {
+//     const url = 'https://data.fixer.io/api/latest?access_key=d78782b1f9fd8844077ef50d3ae21a38&symbols=NGN,GBP,USD';
+
+//     let response = fetch(url)
+
+//     response.then((res)=> {
+//     // console.log(res)
+//         return res.json()
+//     })
+//     .then((data)=>{
+//         return data
+//     })
+// }
